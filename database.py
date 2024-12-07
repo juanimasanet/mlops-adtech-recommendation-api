d4c3870548c7537e909b8a26834d400955580186
juanimasanet <86981535+juanimasanet@users.noreply.github.com>
2024-12-07 10:25:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,25 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+# Variables de conexión
+db_username = 'postgres'
+db_password = 'GRUPO3RDS2024'
+db_host = 'grupo-3-25-11.cf4i6e6cwv74.us-east-1.rds.amazonaws.com'
+db_port = '5432'
+db_name = 'grupo_3'
+
 def get_db_connection():
+    """
+    Establece y devuelve una conexión a la base de datos PostgreSQL.
+    """
     try:
         conn = psycopg2.connect(
-            host="DB_HOST",
-            database="DB_NAME",
-            user="DB_USER",
-            password="DB_PASSWORD",
-            cursor_factory=RealDictCursor
+            host=db_host,
+            database=db_name,
+            user=db_username,
+            password=db_password,
+            port=db_port,
+            cursor_factory=RealDictCursor  
         )
         return conn
     except Exception as e:
