cbfce3d600e612128340dfe2881c65fa5fcd844b
juanimasanet <86981535+juanimasanet@users.noreply.github.com>
2024-12-07 10:36:07 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
-class Recommendation(BaseModel):
+# Modelo para la tabla `top_products_df`
+class TopProduct(BaseModel): #FALTA MODIFICAR CON RESULTADO DAG
     product_id: int
     product_name: str
+    advertiser: str
     score: float
+    model: str  
 
-class Stats(BaseModel):
-    advertiser_count: int
-    top_advertisers: List[str]
-    model_agreement: float
+# Modelo para la tabla `top_ctr_df`
+class TopCTR(BaseModel):
+    advertiser: str
+    product_id: int
+    clicks: int
+    impressions: int
+    ctr: float  # Click-through rate calculado
