3aa0db0eee2de58addb3ee259c55e822cf0b11c3
juanimasanet <86981535+juanimasanet@users.noreply.github.com>
2024-12-08 10:03:23 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routers import recommendations, stats, history
+
+app = FastAPI()
+
+# Registrar los routers
+app.include_router(recommendations.router)
+app.include_router(stats.router)
+app.include_router(history.router)
+
+@app.get("/")
+def root():
+    return {"message": "AdTech Recommendation API"}
